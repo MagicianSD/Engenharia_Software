9271729bd30213840a50cf573f872f1cc5191d3d
LuizFernando82 <luizfernandos1silva@gmai.com>
2021-11-07 15:15:29 -0300

    commit LF

---
+++ Luiz_Fernando/comandos_git.bat
@@ -0,0 +1,9 @@
+git status -> mostra arquivos que não estão preparados pra commit;
+git add . -> prepara todos arquivos para serem comitados;
+git commit -> comita os arquivos que foram preparados (-m -> possibilita comentar);
+git log -> mostra o id do commit e o comentario;
+git rm --cached e nome do arquivo retira o arquivo de preparo para commit;
+git clone endereço do repositorio no github ->clona repositório do github para maquina local; 
+git checkout nome da nova branch ->criamos uma nova branch de trabalho;
+git push origin nome da branch -> manda os arquivos para o github;
+git merge nome do branch -> subir novos arquivos para o checkout;
