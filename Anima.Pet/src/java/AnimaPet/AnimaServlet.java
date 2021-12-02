2fa58691fbca4a77500b0f1a4f76d1263a555dac
Eduardo Silva <54422509+MagicianSD@users.noreply.github.com>
2021-12-02 19:02:34 -0300

    Add files via upload

---
+++ Anima.Pet/src/java/AnimaPet/AnimaServlet.java
@@ -0,0 +1,50 @@
+
+package AnimaPet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class AnimaServlet extends HttpServlet {
+
+   
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+             String name = request.getParameter("userName");
+             out.println("Hello"+name);
+             out.close();
+
+
+        }
+    }
+
+  
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+  
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+   
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+
+}
